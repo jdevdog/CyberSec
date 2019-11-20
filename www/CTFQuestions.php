10db69ade4e325bee6ae042011449db577d38f10
Campola2211 <campola2211@hotmail.com>
2019-11-20 12:43:43 -0500

    commenting out stuff

--- www/CTFQuestions.php
+++ www/CTFQuestions.php
@@ -80,35 +80,35 @@
 	</div>
 		<br>
 <!--<start of php
-include "credentials.php";
+//include "credentials.php";
 	
 	// Create connection
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	//$conn = mysqli_connect($servername, $username, $password, $dbname);
 	// Check connection
-	if ($conn-> connect_error) {
-		die("Connection failed: " . $conn-> connect_error);
-	}
+	//if ($conn-> connect_error) {
+	//	die("Connection failed: " . $conn-> connect_error);
+	//}
 				
 	//fetching all teams and looping through the rows
-	$sql = "SELECT title, text, points, answer, max_attempts FROM questions order by points";
-	$result = mysqli_query($conn, $sql);  		
-	$countID = 0;
-	if ($result-> num_rows > 0) {
-		while($row = $result-> fetch_assoc()) {
-			echo "<div id=\"".$counter."\" class=\"modal\">";
+	//$sql = "SELECT title, text, points, answer, max_attempts FROM questions order by points";
+	//$result = mysqli_query($conn, $sql);  		
+	//$countID = 0;
+	//if ($result-> num_rows > 0) {
+		//while($row = $result-> fetch_assoc()) {
+			//echo "<div id=\"".$counter."\" class=\"modal\">";
   			//<!-- Modal content -->
-			echo "<div class=\"modal-content\">";
-    		echo "<span class=\"close\">&times;</span>";
-    		echo "<p id=\"modalq\">".$row["text"]."</p>";
-    		echo "<p id=\"tries\">".$row["max_attempts"]."</p>";
-    		echo "<input type=\"text\" id=\"answer\" placeholder=\"Answer\">";
-    		echo "<input type=\"button\" value=\"Submit\" id=\"btn\"  onclick=\"javascript:validate()\">";
-    		echo "</div>"
-    		++$counter;
-    	}
-    }
-    $conn-> close();
-end of php>-->
+			//echo "<div class=\"modal-content\">";
+    		//echo "<span class=\"close\">&times;</span>";
+    		//echo "<p id=\"modalq\">".$row["text"]."</p>";
+    		//echo "<p id=\"tries\">".$row["max_attempts"]."</p>";
+    		//echo "<input type=\"text\" id=\"answer\" placeholder=\"Answer\">";
+    		//echo "<input type=\"button\" value=\"Submit\" id=\"btn\"  onclick=\"javascript:validate()\">";
+    		//echo "</div>"
+    		//++$counter;
+    	//}
+    //}
+    //$conn-> close();
+//end of php>-->
 <!-- The Modal -->
 <div id="myModal" class="modal">
 
