40910809a215e1aabde7cf8ec6933f33267cc88a
Campola2211 <campola2211@hotmail.com>
2019-11-20 11:36:50 -0500

    Changed " to '

--- www/CTFQuestions.php
+++ www/CTFQuestions.php
@@ -181,7 +181,7 @@ window.onclick = function(event) {
 						++$rowCount;
 						echo "<div class=\"column odd1\" style=\"background-color:#04315a;\">";
 						echo "<h2>".$row["title"]." (".$row["points"]."xp)</h2>";
-						echo "<button class=\"qbtn\" id=\"".$row["title"]."\" onclick=\"javascript:showQ(this.id, \"".$row["text"]."\")\">Open Question</button>";
+						echo "<button class=\"qbtn\" id=\"".$row["title"]."\" onclick=\"javascript:showQ(this.id, \'".$row["text"]."\')\">Open Question</button>";
 						echo "</div>";
 						if ($rowCount == 3){
 							echo "</div>";
