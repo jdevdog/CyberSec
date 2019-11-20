281230c89af719fd8a789d60f79bba7b1bcade0c
Campola2211 <campola2211@hotmail.com>
2019-11-20 11:13:17 -0500

    Fixing one click issue

--- www/CTFQuestions.php
+++ www/CTFQuestions.php
@@ -137,9 +137,9 @@ var btn = document.getElementById(currentID);
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+
+modal.style.display = "block";
+
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
