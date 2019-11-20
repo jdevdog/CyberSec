b3229823101dfa6dea40cad15eb42c25b879505d
Campola2211 <campola2211@hotmail.com>
2019-11-20 10:18:00 -0500

    Added more lines to showQ to test opening the modal

--- www/CTFQuestions.php
+++ www/CTFQuestions.php
@@ -127,6 +127,33 @@ function validate()
 function showQ(currentID)
 {
 	alert(currentID);
+	/* Code from https://www.w3schools.com/howto/howto_css_modals.asp October 20th */
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById(currentID);
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+/* end of Code from https://www.w3schools.com/howto/howto_css_modals.asp October 20th */
 }
 </script>
 <!--end of code from https://stackoverflow.com/questions/4825295/javascript-onclick-to-get-the-id-of-the-clicked-button-->
@@ -200,33 +227,7 @@ function showQ(currentID)
 			</div> -->
 </div>
 <script>
-/* Code from https://www.w3schools.com/howto/howto_css_modals.asp October 20th */
-// Get the modal
-var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-/* end of Code from https://www.w3schools.com/howto/howto_css_modals.asp October 20th */
 </script>
 
 	</body>
