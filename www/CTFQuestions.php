c108c1dda578058b6a1a5084e663a3cddf3d3f36
Campola2211 <campola2211@hotmail.com>
2019-11-25 15:02:57 -0500

    Form action

--- www/CTFQuestions.php
+++ www/CTFQuestions.php
@@ -130,9 +130,9 @@
     					echo "<span class=\"close\">&times;</span>";
     					echo "<p id=\"modalq\">".$row["text"]."</p>";
     					echo "<p id=\"tries\">"."Attempts Left: ".$row["max_attempts"]."</p>";
-    					echo "<form action="" method=\"post\">"
-    					echo "<label>Answer:</label> <input type=\"text\" id=\"answer\" placeholder=\"Answer\" name=\"Answer\"><br><br>"
-    					echo "<input type=\"submit\" value=\"Submit\" id=\"btn\">"
+    					echo "<form action="" method=\"post\">";
+    					echo "<label>Answer:</label> <input type=\"text\" id=\"answer\" placeholder=\"Answer\" name=\"Answer\"><br><br>";
+    					echo "<input type=\"submit\" value=\"Submit\" id=\"btn\">";
     					//echo "<input type=\"text\" id=\"answer\" placeholder=\"Answer\">";
     					//echo "<input type=\"button\" value=\"Submit\" id=\"btn\"  onclick=\"javascript:validate()\">";
     					echo "</div>";
